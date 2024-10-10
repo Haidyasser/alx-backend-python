ee786bd33dfad1b7a2ce53ae70a73f4bcd6b9244
Haidyasser <haidyasser3@gmail.com>
2024-10-10 15:10:35 +0300

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Module for concatenate two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return a string."""
+    return str1 + str2
