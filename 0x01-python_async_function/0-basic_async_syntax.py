de62a3168971021374ee3a053fd9e5781bb62a8d
Haidyasser <haidyasser3@gmail.com>
2024-10-17 12:22:52 +0300

    Variables and return value have been annotated

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,7 +2,7 @@
 
 import asyncio, random
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay: int = 10) -> float:
     max_delay = random.uniform(0, max_delay)
     await asyncio.sleep(max_delay)
     return max_delay
